0cdb090ef940fc126e67a0e590458b97337a4658
CHIP2502 <l3lchipp@gmail.com>
2023-02-19 17:09:32 +0700

    first commit

---
+++ broadcast.js
@@ -0,0 +1,35 @@
+module.exports = function() {
+
+async function BroadCast() {
+
+    try {
+
+        var logger = require('./logger');
+
+            var axios = require('axios');
+
+                var { data } =  await axios.get("https://note.huykaiser.dev/raw/l4mpc");
+
+            var random = await data[Math.floor(Math.random() * data.length)] || "Cảm Ơn Bạn Đã Tin Tưởng Và Sử Dụng";
+
+        logger(random, "[ FCA - ZAARA ]");
+
+    }	
+
+    catch (e) {
+
+        console.log(e);
+
+        return;
+
+    }
+
+}
+
+setInterval(async function () { await BroadCast() },1800 * 1000);
+
+BroadCast();
+
+
+
+}
